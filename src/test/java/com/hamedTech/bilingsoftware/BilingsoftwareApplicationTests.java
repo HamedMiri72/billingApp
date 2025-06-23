c869ade6a12b91a230ad7e43ea672064afdc3503
hamed miri <hamedmiri@hameds-MacBook-Air.local>
2025-06-23 17:55:03 +0100

    first commit

---
+++ src/test/java/com/hamedTech/bilingsoftware/BilingsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamedTech.bilingsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
